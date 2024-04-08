5202e625ce99a7fd44e17a0f86241826a8e98ab9
nirosha <niroshakannan17@gmail.com>
2024-04-08 17:50:37 +0530

    commit

--- pipe/src/app/app.component.ts
+++ pipe/src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
+
 import { RouterOutlet } from '@angular/router';
+
 
 @Component({
   selector: 'app-root',
@@ -10,4 +12,6 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'pipe';
+
+  dateObj = new Date();
 }
